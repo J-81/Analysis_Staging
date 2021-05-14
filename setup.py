a6bb4c4dfbc42824356dd13a0d447d549c7f1660
J-81 <jonathan.d.oribello@gmail.com>
2021-05-14 10:50:55 -0700

    Updating version. New alternative template feature added.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ dirname = os.path.dirname(__file__)
 
 setup(
    name='AST',
-   version='0.3.1-dev',
+   version='0.3.2-dev',
    description='Tools for staging and setting up samplesheets.',
    author='Jonathan Oribello',
    author_email='jonathan.d.oribello@gmail.com',
